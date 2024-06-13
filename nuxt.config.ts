e295457bb3dd2276e2a277620cc7bd47cd84789e
amandesai01 <amandesai01@gmail.com>
2024-06-13 22:50:05 +0530

    config: oauth runtime + db

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,37 @@ export default defineNuxtConfig({
     "~/assets/css/additional-styles/utility-patterns.css",
   ],
   devtools: { enabled: true },
+  runtimeConfig: {
+    db: {
+      host: 'localhost',
+      port: 55000,
+      user: 'postgres',
+      password: 'postgrespw',
+      database: 'findjedi-local',
+    },
+    server: {
+      userInfo: 'https://api.thenirvanalabs.com/user/info',
+    },
+    public: {
+      oauth: {
+        endpoints: {
+          authorization: 'https://atlas.thenirvanalabs.com/oauth2/authorize',
+          token: 'https://atlas.thenirvanalabs.com/oauth2/token',
+          userInfo: 'https://api.thenirvanalabs.com/user/me',
+        },
+        redirect: {
+          home: '/',
+          callback: '/login',
+          logout: '/',
+          login: '/login',
+        },
+        clientId: 'base-client',
+        scope: ['openid'],
+        origin: 'http://localhost:3001',
+      },
+      serverBaseURL: 'https://api.thenirvanalabs.com',
+    },
+    basicInfoEndpoint: 'https://api.thenirvanalabs.com/user/basic-profile',
+    jwksEndpoint: 'https://atlas.thenirvanalabs.com/oauth2/jwks',
+  },
 });
