dfa8660fa3f9cee5a9cac18ff276c8d0896c1bd7
amandesai01 <amandesai01@gmail.com>
2024-06-18 03:13:44 +0530

    ui: add admin panel tag

--- components/admin/Header.vue
+++ components/admin/Header.vue
@@ -1,8 +1,10 @@
 <template>
-  <header class="sticky top-0 bg-white dark:bg-zinc-950 border-b border-zinc-200 dark:border-zinc-700 z-30">
+  <header class="sticky top-0 bg-white dark:bg-zinc-950 border-b border-zinc-300 dark:border-zinc-700 z-30">
     <div class="px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between h-16 -mb-px">
-        <img src="/vidur-logo.svg" class="h-8">
+        <div class="flex space-x-2 items-center text-[0.5rem]">
+          <img src="/vidur-logo.svg" class="h-8"><span class="font-extrabold bg-zinc-900 text-white px-2 py-1 rounded-full">Admin Panel</span>
+        </div>
         <!-- Header: Right side -->
         <div class="flex items-center space-x-3">
           <!-- Divider -->
