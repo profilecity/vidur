0f67c591994e3527f21a78920e7e563a805340d1
amandesai01 <amandesai01@gmail.com>
2024-06-18 03:10:39 +0530

    chore: format config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vee-validate/nuxt'],
   css: [
-    "~/assets/css/style.css",
-    "~/assets/css/additional-styles/range-slider.css",
-    "~/assets/css/additional-styles/theme.css",
-    "~/assets/css/additional-styles/toggle-switch.css",
-    "~/assets/css/additional-styles/utility-patterns.css",
+    '~/assets/css/style.css',
+    '~/assets/css/additional-styles/range-slider.css',
+    '~/assets/css/additional-styles/theme.css',
+    '~/assets/css/additional-styles/toggle-switch.css',
+    '~/assets/css/additional-styles/utility-patterns.css',
   ],
   devtools: { enabled: true },
   runtimeConfig: {
