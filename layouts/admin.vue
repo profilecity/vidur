c4688d0df13cdf269595cba7a33f731235482c5c
amandesai01 <amandesai01@gmail.com>
2024-06-18 03:16:25 +0530

    ui: admin layout styling

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -6,11 +6,13 @@ useHead({
 })
 </script>
 <template>
-  <main class="min-h-screen">
+  <main class="min-h-screen w-full">
     <AdminHeader />
-    <div class="flex flex-col md:flex-row md:-mr-px h-full">
-      <AdminSidebar class="h-[calc(100vh-4rem)]" />
-      <slot />
+    <div class="flex flex-col md:flex-row h-full w-full">
+      <AdminSidebar class="md:h-[calc(100vh-4rem)]" />
+      <section class="p-4 w-full">
+        <slot />
+      </section>
     </div>
   </main>
 </template>
