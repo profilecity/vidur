a0e8741e43d9c5e0dd54eeaf01d9fadc764d35a5
amandesai01 <amandesai01@gmail.com>
2024-06-20 19:26:56 +0530

    chore: format and integrate hook

--- server/api/application.post.ts
+++ server/api/application.post.ts
@@ -1,14 +1,14 @@
-import { applicationCreateSchema } from "~/schemas/application";
-import authenticateRequest from "../utils/auth";
-import { and, count, eq, sql } from "drizzle-orm";
-import { jobPostingsTable, postingApplicantsTable } from "../db/schema";
+import { applicationCreateSchema } from '~/schemas/application';
+import authenticateRequest from '../utils/auth';
+import { and, count, eq, sql } from 'drizzle-orm';
+import { jobPostingsTable, postingApplicantsTable } from '../db/schema';
 
 export default defineEventHandler(async (event) => {
   const session = await authenticateRequest(event);
   const body = await readValidatedBody(event, applicationCreateSchema.parse);
 
   if (IS_DEV) {
-    console.log("user", session.user.id, "applying to", body.postingId);
+    console.log('user', session.user.id, 'applying to', body.postingId);
   }
 
   const database = await useDatabase();
@@ -21,18 +21,18 @@ export default defineEventHandler(async (event) => {
         .where(
           and(
             eq(jobPostingsTable.id, body.postingId),
-            eq(jobPostingsTable.isPublished, true)
-          )
+            eq(jobPostingsTable.isPublished, true),
+          ),
         )
     )[0].count == 1;
 
   if (!postingExistsAndIsPublished) {
     if (IS_DEV) {
-      console.log("posting not found.", "applying to", body.postingId);
+      console.log('posting not found.', 'applying to', body.postingId);
     }
     throw createError({
       statusCode: 404,
-      message: "Posting Not Found",
+      message: 'Posting Not Found',
     });
   }
 
@@ -44,23 +44,23 @@ export default defineEventHandler(async (event) => {
         .where(
           and(
             eq(postingApplicantsTable.postingId, body.postingId),
-            eq(postingApplicantsTable.candidateId, session.user.id)
-          )
+            eq(postingApplicantsTable.candidateId, session.user.id),
+          ),
         )
     )[0].count == 1;
 
   if (userAlreadyApplied) {
     if (IS_DEV) {
       console.log(
-        "user already applied. userId",
+        'user already applied. userId',
         session.user.id,
-        "applying to",
-        body.postingId
+        'applying to',
+        body.postingId,
       );
     }
     throw createError({
       statusCode: 400,
-      message: "Already Applied",
+      message: 'Already Applied',
     });
   }
 
@@ -79,11 +79,23 @@ export default defineEventHandler(async (event) => {
   });
 
   if (IS_DEV) {
+    console.log('calling hook:application-create');
+    // Don't await for task.
+    runTask('hooks:application-create', {
+      payload: {
+        applicantId: session.user.id,
+        postingId: body.postingId,
+        bearerToken: session.accessToken,
+      },
+    });
+  }
+
+  if (IS_DEV) {
     console.log(
-      "user",
+      'user',
       session.user.id,
       "'s application successful for",
-      body.postingId
+      body.postingId,
     );
   }
 });
