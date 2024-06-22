796e7943c53ed3b1aa6cc5e0505e7e484b707a32
amandesai01 <amandesai01@gmail.com>
2024-06-22 17:48:23 +0530

    fix(posting): remove owner id constraint till permissions implemented

--- server/api/posting.delete.ts
+++ server/api/posting.delete.ts
@@ -1,7 +1,7 @@
-import { and, eq } from "drizzle-orm";
-import { jobPostingsTable } from "../db/schema";
-import authenticateAdminRequest from "../utils/admin";
-import { deleteJobPostingSchema } from "~/schemas/posting"
+import { eq } from 'drizzle-orm';
+import { jobPostingsTable } from '../db/schema';
+import authenticateAdminRequest from '../utils/admin';
+import { deleteJobPostingSchema } from '~/schemas/posting';
 
 export default defineEventHandler(async (event) => {
   const session = await authenticateAdminRequest(event);
@@ -14,19 +14,14 @@ export default defineEventHandler(async (event) => {
 
   const jobPostingsResult = await database
     .delete(jobPostingsTable)
-    .where(
-      and(
-        eq(jobPostingsTable.id, jobPostingId),
-        eq(jobPostingsTable.owner, session.user.id)
-      )
-    )
+    .where(eq(jobPostingsTable.id, jobPostingId))
     .returning({ id: jobPostingsTable.id });
 
   if (!(Array.isArray(jobPostingsResult) && jobPostingsResult.length == 1)) {
     throw createError({
       statusCode: 400,
       message:
-        "Invalid posting id. Contact support if you think this is a mistake.",
+        'Invalid posting id. Contact support if you think this is a mistake.',
     });
   }
 });
--- server/api/posting.put.ts
+++ server/api/posting.put.ts
@@ -1,7 +1,7 @@
-import { jobPostingsTable } from "../db/schema";
-import authenticateAdminRequest from "../utils/admin";
-import { and, eq } from "drizzle-orm";
-import { updateJobPostingSchema } from "~/schemas/posting";
+import { jobPostingsTable } from '../db/schema';
+import authenticateAdminRequest from '../utils/admin';
+import { eq } from 'drizzle-orm';
+import { updateJobPostingSchema } from '~/schemas/posting';
 
 export default defineEventHandler(async (event) => {
   const database = await useDatabase();
@@ -10,7 +10,7 @@ export default defineEventHandler(async (event) => {
   const q = await readValidatedBody(event, updateJobPostingSchema.parse);
 
   if (IS_DEV) {
-    console.log("updating posting id", q.id);
+    console.log('updating posting id', q.id);
   }
 
   const jobPostingId = q.id;
@@ -18,18 +18,13 @@ export default defineEventHandler(async (event) => {
   const jobPostingsResult = await database
     .select()
     .from(jobPostingsTable)
-    .where(
-      and(
-        eq(jobPostingsTable.id, jobPostingId),
-        eq(jobPostingsTable.owner, session.user.id)
-      )
-    );
+    .where(eq(jobPostingsTable.id, jobPostingId));
 
   if (!(Array.isArray(jobPostingsResult) && jobPostingsResult.length == 1)) {
     throw createError({
       statusCode: 400,
       message:
-        "Invalid posting id. Contact support if you think this is a mistake.",
+        'Invalid posting id. Contact support if you think this is a mistake.',
     });
   }
 
@@ -38,13 +33,15 @@ export default defineEventHandler(async (event) => {
   if (oldJobPosting.owner != session.user.id) {
     throw createError({
       statusCode: 400,
-      message: "Provided owner is not authorised to update this job posting",
+      message: 'Provided owner is not authorised to update this job posting',
     });
   }
 
   if (!oldJobPosting.isPublished && q.isPublished) {
     if (IS_DEV) {
-      console.log("posting " + q.id + " published by " + session.user.firstName);
+      console.log(
+        'posting ' + q.id + ' published by ' + session.user.firstName,
+      );
     }
   }
 
