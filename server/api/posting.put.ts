9771a78a9de7b887fb0238ee1844d0fd346e4152
amandesai01 <amandesai01@gmail.com>
2024-06-22 18:01:46 +0530

    fix(posting): remove owner id constraint till permissions implemented

--- server/api/posting.put.ts
+++ server/api/posting.put.ts
@@ -6,43 +6,11 @@ import { updateJobPostingSchema } from '~/schemas/posting';
 export default defineEventHandler(async (event) => {
   const database = await useDatabase();
 
-  const session = await authenticateAdminRequest(event);
+  await authenticateAdminRequest(event);
   const q = await readValidatedBody(event, updateJobPostingSchema.parse);
 
   if (IS_DEV) {
     console.log('updating posting id', q.id);
-  }
-
-  const jobPostingId = q.id;
-
-  const jobPostingsResult = await database
-    .select()
-    .from(jobPostingsTable)
-    .where(eq(jobPostingsTable.id, jobPostingId));
-
-  if (!(Array.isArray(jobPostingsResult) && jobPostingsResult.length == 1)) {
-    throw createError({
-      statusCode: 400,
-      message:
-        'Invalid posting id. Contact support if you think this is a mistake.',
-    });
-  }
-
-  const oldJobPosting = jobPostingsResult[0];
-
-  if (oldJobPosting.owner != session.user.id) {
-    throw createError({
-      statusCode: 400,
-      message: 'Provided owner is not authorised to update this job posting',
-    });
-  }
-
-  if (!oldJobPosting.isPublished && q.isPublished) {
-    if (IS_DEV) {
-      console.log(
-        'posting ' + q.id + ' published by ' + session.user.firstName,
-      );
-    }
   }
 
   const updateQuery = {
