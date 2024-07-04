a92163a5424361db6412f29eb6ccba1ae793ef32
devshahofficial <devshahofficial001@gmail.com>
2024-07-04 22:25:33 +0530

    header removed

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,8 +1,7 @@
 <template>
   <main class="min-h-screen w-full">
-    <AdminHeader />
     <div class="flex flex-col md:flex-row h-full w-full">
-      <AdminSidebar class="md:h-[calc(100vh-4rem)]" />
+      <AdminSidebar/>
       <section class="px-4 py-2 md:px-4 md:py-4 w-full">
         <slot />
       </section>
