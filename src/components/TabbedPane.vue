eb2d714e0d56fb79d96e440d40f7a5f375348f6e
amandesai01 <amandesai01@gmail.com>
2024-07-05 21:20:53 +0530

    generalise tabbed pane

---
+++ src/components/TabbedPane.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type Tab = {
+  id: string;
+  display: string;
+  icon: string;
+  url: string;
+}
+
+defineProps<{
+  tabs: Tab[];
+  activeTab: string;
+}>();
+</script>
+
+<template>
+  <div class="flex items-center">
+    <div class="bg-zinc-100 flex p-1 space-x-1 rounded-xl border border-zinc-200">
+      <NuxtLink :to="tab.url" v-for="tab in tabs" class="flex items-center px-2.5 py-2 text-zinc-500 rounded-xl"
+        :class="activeTab == tab.id ? 'text-zinc-950 bg-white border border-zinc-200' : 'hover:bg-zinc-50'">
+        <Icon class="w-5 h-5 shrink-0 fill-current mr-2" :name="tab.icon" />
+        <span class="text-sm" :class="activeTab == tab.id ? 'font-semibold' : ''">{{ tab.display }}</span>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
--- src/components/admin/settings/Header.vue
+++ src/components/admin/settings/Header.vue
@@ -12,31 +12,20 @@ const tabs = [
     id: "general",
     display: "General",
     icon: "codicon:settings",
+    url: '/admin/settings?tab=general',
   },
   {
     id: "members",
     display: "Members",
     icon: "fluent:people-20-regular",
+    url: '/admin/settings?tab=members',
   }
 ]
 </script>
 
 <template>
-  <div class="flex text-xl font-bold text-zinc-900 px-4">Settings</div>
-  <ul class="flex space-x-2 border-b pb-4 items-center mt-2 mb-4 px-4">
-    <div class="bg-zinc-100 flex p-1 space-x-1 rounded-xl border border-zinc-200">
-      <NuxtLink :to="`/admin/settings?tab=${tab.id}`" v-for="tab in tabs" custom
-      v-slot="{ href, navigate }">
-      <li class="mr-0.5 md:mr-0">
-        <a class="flex items-center px-2.5 py-2 text-zinc-500"
-          :class="activeTab == tab.id ? 'text-zinc-950 rounded-xl bg-white border border-zinc-200' : 
-          'hover:bg-zinc-50 hover:rounded-xl'" 
-          :href="href" @click="navigate">
-          <Icon class="w-5 h-5 shrink-0 fill-current mr-2" :name="tab.icon" />
-          <span class="text-sm" :class="activeTab == tab.id ? 'font-semibold' : ''">{{ tab.display }}</span>
-        </a>
-      </li>
-    </NuxtLink>
-    </div>
-  </ul>
+  <section class="sticky top-0 bg-white p-4">
+    <div class="flex text-xl font-bold text-zinc-900">Settings</div>
+    <TabbedPane :active-tab="activeTab" :tabs="tabs" class="border-b py-4 mt-2" />
+  </section>
 </template>
