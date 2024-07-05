1007be9cfe6a8056ab9667bf161a45ef6f024b8b
devshahofficial <devshahofficial001@gmail.com>
2024-07-05 19:00:48 +0530

    layout fixed

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -2,7 +2,7 @@
   <main class="min-h-screen w-full">
     <div class="flex flex-col md:flex-row h-full w-full">
       <AdminSidebar/>
-      <section class="px-4 py-2 md:px-4 md:py-4 w-full">
+      <section class="ml-60 py-2 md:py-4 w-full">
         <slot />
       </section>
     </div>
