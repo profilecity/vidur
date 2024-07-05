e6cffc87c34c12a71e2ed475e346e1d8312b58a9
devshahofficial <devshahofficial001@gmail.com>
2024-07-05 19:01:50 +0530

    header fixed

--- src/components/admin/settings/Header.vue
+++ src/components/admin/settings/Header.vue
@@ -11,27 +11,32 @@ const tabs = [
   {
     id: "general",
     display: "General",
-    icon: "mingcute:settings-2-line",
+    icon: "codicon:settings",
   },
   {
     id: "members",
     display: "Members",
-    icon: "ph:users-bold",
+    icon: "fluent:people-20-regular",
   }
 ]
 </script>
 
 <template>
-  <ul class="flex flex-nowrap space-x-2 overflow-x-scroll no-scrollbar pb-2 border-b">
-    <NuxtLink :to="`/admin/settings?tab=${tab.id}`" v-for="tab in tabs" custom
+  <div class="flex text-lg font-bold text-zinc-900 px-4">Settings</div>
+  <ul class="flex space-x-2 border-b pb-4 items-center mt-2 mb-4 px-4">
+    <div class="bg-zinc-100 flex p-1 space-x-1 rounded-xl border border-zinc-200">
+      <NuxtLink :to="`/admin/settings?tab=${tab.id}`" v-for="tab in tabs" custom
       v-slot="{ href, navigate }">
-      <li class="mr-0.5 md:mr-0 md:mb-0.5">
-        <a class="flex items-center px-2.5 py-2 rounded-xl whitespace-nowrap"
-          :class="activeTab == tab.id ? 'bg-zinc-950 text-white border' : 'hover:bg-zinc-200'" :href="href" @click="navigate">
-          <Icon class="w-4 h-4 shrink-0 fill-current mr-2" :name="tab.icon" />
-          <span class="text-sm font-medium">{{ tab.display }}</span>
+      <li class="mr-0.5 md:mr-0">
+        <a class="flex items-center px-2.5 py-2 text-zinc-500"
+          :class="activeTab == tab.id ? 'text-zinc-950 rounded-xl bg-white border border-zinc-200' : 
+          'hover:bg-zinc-50 hover:rounded-xl'" 
+          :href="href" @click="navigate">
+          <Icon class="w-5 h-5 shrink-0 fill-current mr-2" :name="tab.icon" />
+          <span class="text-sm" :class="activeTab == tab.id ? 'font-semibold' : ''">{{ tab.display }}</span>
         </a>
       </li>
     </NuxtLink>
+    </div>
   </ul>
 </template>
