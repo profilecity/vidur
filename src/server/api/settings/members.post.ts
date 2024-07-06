e97e960039818e09f92f6b5f3fd64c01ade8db40
amandesai01 <amandesai01@gmail.com>
2024-07-06 14:50:32 +0530

    feat: members api

---
+++ src/server/api/settings/members.post.ts
@@ -0,0 +1,14 @@
+import { eq } from 'drizzle-orm';
+import { addMemberSchema } from '~/schemas/setting';
+import { usersTable } from '~/server/db/schema';
+import authenticateAdminRequest from '~/server/utils/admin';
+
+export default defineEventHandler(async (event) => {
+  await authenticateAdminRequest(event);
+
+  const request = await readValidatedBody(event, addMemberSchema.parse);
+
+  const db = await useDatabase();
+
+  await db.update(usersTable).set({ isAdmin: true }).where(eq(usersTable.id, request.id));
+});
