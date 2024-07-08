f5ad4af53cc0c7a1b26d75cb5248f74745955f45
devshahofficial <devshahofficial001@gmail.com>
2024-07-08 13:50:49 +0530

    firstname

--- src/pages/admin/dashboard.vue
+++ src/pages/admin/dashboard.vue
@@ -9,13 +9,14 @@ useHead({
 })
 
 const stats = await useFetch('/api/analytics/stats');
-const activePostings = await useFetch('/api/analytics/job');
+const { user } = await useAuth();
+const firstName = user.value?.profile.firstName;
 </script>
 
 <template>
   <div class="px-4 py-4">
     <div class=" pb-4 mb-4 border-b border-zinc-200">
-      <div class="text-xl font-bold text-zinc-900">👋🏽 Hello, Dev Shah.</div>
+      <div class="text-xl font-bold text-zinc-900">👋🏽 Hello, {{ firstName }}</div>
       <div class="text-sm text-zinc-500 mt-2">Track your hiring activities. You are almost there!</div>
     </div>
     <AdminDashboardStats :stats="stats.data.value" v-if="stats.data.value"/>
