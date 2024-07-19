06df8ec95dc55cd954acedd5d276abf684510ceb
devshahofficial <devshahofficial001@gmail.com>
2024-07-19 08:44:22 +0530

    UI-changes

--- src/components/admin/postings/Header.vue
+++ src/components/admin/postings/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="sticky top-0 bg-white p-4">
+  <section class="sticky top-0 bg-white p-4 border-b border-zinc-200">
     <div class="flex text-xl font-bold text-zinc-900">Postings</div>
-    <div class="flex items-center border-b py-4 mt-2">
+    <div class="flex items-center mt-2">
       <NuxtLink to="/admin/postings/edit"
           class="btn text-zinc-900 hover:border-zinc-300 hover:text-zinc-800">
           <span class="mr-2">Create Posting</span>
