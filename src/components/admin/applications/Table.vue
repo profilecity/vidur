18384bbac371b17faec15827dd216b14148883a1
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2024-07-22 13:17:12 +0530

    chore: ui: applicants UI refactor (#18)

--- src/components/admin/applications/Table.vue
+++ src/components/admin/applications/Table.vue
@@ -7,32 +7,14 @@ defineProps<{
 
 <template>
   <div class="overflow-x-auto">
-    <table class="table-auto w-full">
-      <!-- Table header -->
-      <thead
-        class="text-xs font-semibold uppercase text-gray-500 border-y border-gray-200">
-        <tr>
-          <th class="px-2 first:pl-5 last:pr-5 py-1 whitespace-nowrap">
-            <div class="font-semibold text-left">Applicant</div>
-          </th>
-          <th class="px-2 first:pl-5 last:pr-5 py-1 whitespace-nowrap">
-            <div class="font-semibold text-left">Public Handles</div>
-          </th>
-          <th class="px-2 first:pl-5 last:pr-5 py-1 whitespace-nowrap">
-            <div class="font-semibold text-left">Applied</div>
-          </th>
-          <th class="px-2 first:pl-5 last:pr-5 py-1 whitespace-nowrap">
-            <div class="font-semibold text-right">Resume</div>
-          </th>
-        </tr>
-      </thead>
+    <section class="w-full">
       <!-- Table body -->
-      <tbody
-        class="text-sm divide-y divide-gray-100 border-b border-gray-200">
+      <div
+        class="text-sm">
         <AdminApplicationsRow v-for="application in applications" :key="application.id"
           :applicant="applicants[application.candidateId]" :application="application" />
-      </tbody>
-    </table>
+      </div>
+    </section>
 
   </div>
 </template>
--- src/pages/admin/applications.vue
+++ src/pages/admin/applications.vue
@@ -46,8 +46,10 @@ watchEffect(() => {
   <AdminApplicationsHeader :posting-ids="selectedPostings" :postings @postings-selected="onPostingsSelected" />
   <section class="p-4"v-if="selectedPostings.length > 0">
     <section class="mb-6" v-for="posting in selectedPostings" :key="posting">
-      <div class="text-md" v-if="applications[posting]">{{ postingsById[posting] || "" }} <span class="text-zinc-500 text-sm">({{ applications[posting]?.length }} Applicants)</span></div>
-      <AdminApplicationsTable class="mt-2" :applicants="applicants" :applications="applications[posting]" v-if="applications[posting]"/>
+      <div class="text-md" v-if="applications[posting]">
+        <span class="font-noto font-bold mr-2">{{ postingsById[posting] || "" }}</span> <span class="bg-blue-100 px-2 py-1 border border-blue-300 rounded-lg text-blue-700 text-sm">{{ applications[posting]?.length }} Applicants</span>
+      </div>
+      <AdminApplicationsTable class="mt-4" :applicants="applicants" :applications="applications[posting]" v-if="applications[posting]"/>
     </section>
   </section>
   <AdminApplicationsEmptyState class="max-w-2xl mx-auto mt-12" v-else/>
