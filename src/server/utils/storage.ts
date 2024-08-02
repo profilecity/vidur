6c1ca82ee11777be43771807c24239bd5ff3ab75
amandesai01 <amandesai01@gmail.com>
2024-08-02 20:43:31 +0530

    feat: inmemory caching

--- src/server/utils/storage.ts
+++ src/server/utils/storage.ts
@@ -1,2 +1,5 @@
 export const BLOB_STORAGE_KEY = "blobStorage";
 export const blobStorage = useStorage(BLOB_STORAGE_KEY);
+
+export const MEMORY_STORAGE_KEY = "memoryStorage";
+export const memoryStorage = useStorage(MEMORY_STORAGE_KEY);
