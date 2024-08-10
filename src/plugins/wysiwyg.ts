c8666a804c761ffca0b199d0908aad024f25f48e
pramaths <103276292+pramaths@users.noreply.github.com>
2024-08-11 01:19:05 +0530

    removed pack-lock

--- src/plugins/wysiwyg.ts
+++ src/plugins/wysiwyg.ts
@@ -1,5 +1,5 @@
 import { defineNuxtPlugin } from '#app'
-import { useWysiwygEditor } from '../../../composables/useWysiwygEditor'
+import { useWysiwygEditor } from '../composables/useWysiwygEditor'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.provide('wysiwygEditor', useWysiwygEditor)
