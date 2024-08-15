e99e71fcdc5bb66404c50be8d5006a8771953b95
amandesai01 <amandesai01@gmail.com>
2024-08-16 00:57:20 +0530

    feat: update settings schema with further fields

--- src/schemas/setting.ts
+++ src/schemas/setting.ts
@@ -1,29 +1,38 @@
 import { z } from 'zod';
 
-export const seoConfigSchema = z
-  .object({
-    favicon: z.string().uuid().nullable().optional(),
-    title: z.string().max(60).nullable().optional(), // Std. recommended size.
-    description: z.string().max(110).nullable().optional(), // Std. recommended size. (To not cutoff in mobile display)
-    twitter: z.string().max(15).nullable().optional(), // Official twitter handle size.
-  })
+export const seoConfigSchema = z.object({
+  favicon: z.string().uuid().nullable().optional(),
+  title: z.string().max(60).nullable().optional(), // Std. recommended size.
+  description: z.string().max(110).nullable().optional(), // Std. recommended size. (To not cutoff in mobile display)
+  twitter: z.string().max(15).nullable().optional(), // Official twitter handle size.
+});
 
 export type SEOConfig = z.infer<typeof seoConfigSchema>;
 
-export const organizationConfigSchema = z
-  .object({
-    name: z.string().max(36).min(1),
-    description: z.string().nullable().optional(),
-    location: z.string().max(24).nullable().optional(),
-    logo: z.string().uuid(),
-    links: z
-      .array(
-        z.object({
-          title: z.string().max(24),
-          href: z.string().url(),
-        }),
-      ),
-  })
+export const organizationConfigSchema = z.object({
+  name: z.string().max(36).min(1),
+  bio: z.string().max(120).min(0).optional(), // Standard one-liner to show as tagline.
+  description: z.string().nullable().optional(), // Proper markdown enabled bio page.
+  location: z.string().max(24).nullable().optional(),
+  logo: z.string().uuid(),
+  overview: z.object({
+    socials: z.array(
+      z.object({
+        handle: z.string().max(24),
+        href: z.string().url(),
+      }),
+    ),
+    companySize: z.number().max(5).min(0).optional(), // [1-10, 10-50, 50-200, 200-1k, 1k-5k, 5k+]
+    totalRaised: z.string().max(20).optional(),
+    markets: z.string().max(120).min(0).optional(),
+  }),
+  links: z.array(
+    z.object({
+      title: z.string().max(24),
+      href: z.string().url(),
+    }),
+  ),
+});
 
 export type OrganizationConfig = z.infer<typeof organizationConfigSchema>;
 
