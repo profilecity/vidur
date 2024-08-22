1e8748e925c7035ef052e54066ea90b0762237f4
amandesai01 <amandesai01@gmail.com>
2024-08-22 17:19:02 +0530

    chore(ui): Migrate Modal to radix-vue `Dialog`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,6 +48,20 @@ module.exports = {
         wider: '0.02em',
         widest: '0.4em',
       },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+        },
+      },
+      animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [
