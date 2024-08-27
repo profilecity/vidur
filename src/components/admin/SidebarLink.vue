39de9d42eecd7c1efd68333bba4c00a4f97d92f7
amandesai01 <amandesai01@gmail.com>
2024-08-27 17:50:55 +0530

    fix(ui): update sidebar link to work with nuxt 13

--- src/components/admin/SidebarLink.vue
+++ src/components/admin/SidebarLink.vue
@@ -7,14 +7,20 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="to" custom v-slot="{ href, navigate, isExactActive }">
-    <li class="mr-0.5 md:mr-0 md:mb-0.5">
-      <a class="flex items-center px-2.5 py-2 rounded-xl whitespace-nowrap text-zinc-600"
-        :class="isExactActive ? 'bg-zinc-100 text-zinc-900 border font-noto' : 'hover:bg-zinc-50 hover:border hover:border-zinc-100'"
-        :href="href" @click="navigate">
-        <Icon class="w-5 h-5 shrink-0 fill-current mr-2" :name="icon" />
-        <span class="text-sm" :class="isExactActive ? 'font-semibold' : ''">{{ label }}</span>
-      </a>
-    </li>
-  </NuxtLink>
+  <li class="mr-0.5 md:mr-0 md:mb-0.5">
+    <NuxtLink :to="to" class="link" activeClass="link-active">
+      <Icon class="w-5 h-5 shrink-0 fill-current mr-2" :name="icon" />
+      <span class="text-sm">{{ label }}</span>
+    </NuxtLink>
+  </li>
 </template>
+
+<style scoped>
+.link {
+  @apply flex items-center px-2.5 py-2 rounded-xl whitespace-nowrap text-zinc-600 hover:bg-zinc-50 hover:border hover:border-zinc-100
+}
+
+.link-active {
+  @apply bg-zinc-100 text-zinc-900 border font-noto font-semibold
+}
+</style>
