9b999e291263abde21bc05f9ee67ab5d286126e6
amandesai01 <amandesai01@gmail.com>
2024-08-27 20:07:51 +0530

    chore(ui): styling for dialog title and desc

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -34,8 +34,8 @@ const close = () => {
     <DialogPortal>
       <DialogOverlay class="bg-zinc-900 bg-opacity-60 backdrop-blur-sm data-[state=open]:animate-overlayShow fixed inset-0 z-30"/>
       <DialogContent class="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-[100]">
-        <DialogTitle v-if="title">{{ title }}</DialogTitle>
-        <DialogDescription v-if="description">{{ description }}</DialogDescription>
+        <DialogTitle class="font-bold text-zinc-900 font-noto" v-if="title">{{ title }}</DialogTitle>
+        <DialogDescription class="text-sm text-zinc-500" v-if="description">{{ description }}</DialogDescription>
         <slot name="content" :close="close" />
         <DialogClose class="absolute top-[10px] right-[10px] inline-flex items-center justify-center">
           <InputButton variant="ghost" size="icon" @click.stop="close">
