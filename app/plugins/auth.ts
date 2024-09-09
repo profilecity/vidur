a9d3690f774f5a601aa8e3554359d27df879498a
amandesai01 <amandesai01@gmail.com>
2024-09-09 20:47:00 +0530

    fix(auth)!: silently ignore auth to support unauthenticated pages

--- app/plugins/auth.ts
+++ app/plugins/auth.ts
@@ -6,15 +6,8 @@ export default defineNuxtPlugin(async () => {
   try {
     const res = await useAsyncData<Session>('oauth-sess-fetch', () => requestFetch('/api/userinfo'));
     const fetchedSession = res.data.value;
-    if (!fetchedSession) {
-      throw new Error('Empty session received');
-    }
-    session.value = fetchedSession;
+    session.value = fetchedSession as Session; // it's ok if data not present.
   } catch (e) {
-    console.error('Error fetching user.', e);
-    throw createError({
-      statusCode: 500,
-      statusMessage: 'Unable to setup session. Try again later',
-    });
+    console.error('Error while fetching session', e);
   }
 });
