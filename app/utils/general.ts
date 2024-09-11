c116727ed3c3fe7d1f8e60fbf5711436db534990
HemalDesai <hemald332@gmail.com>
2024-09-11 23:59:55 +0530

    Update date formatting to display minutes, hours, days, and months based on elapsed time

--- app/utils/general.ts
+++ app/utils/general.ts
@@ -29,26 +29,27 @@ export const sleep = (ms: number) => {
 
 export function timeAgo(date: Date) {
   const now = new Date();
-  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+  const postedDate = new Date(date.toISOString().replace('Z', '')).getTime();
+  const seconds = Math.floor((now.getTime() - postedDate) / 1000);
 
   let interval = Math.floor(seconds / 31536000);
-  if (interval > 1) {
+  if (interval >= 1) {
     return `${interval}y ago`;
   }
   interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
+  if (interval >= 1) {
     return `${interval}mo ago`;
   }
   interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
+  if (interval >= 1) {
     return `${interval}d ago`;
   }
   interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
+  if (interval >= 1) {
     return `${interval}h ago`;
   }
   interval = Math.floor(seconds / 60);
-  if (interval > 1) {
+  if (interval >= 1) {
     return `${interval}m ago`;
   }
   return `Just Now`;
