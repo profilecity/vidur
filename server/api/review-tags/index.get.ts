01e35bbbe73e803198326831baf11621b524dfbd
Aman Desai <39585600+amandesai01@users.noreply.github.com>
2024-09-16 20:50:05 +0530

    Update server/api/review-tags/index.get.ts

--- server/api/review-tags/index.get.ts
+++ server/api/review-tags/index.get.ts
@@ -1,5 +1,4 @@
 import { type ReviewTag } from '../../db/schema';
-import authenticateAdminRequest from '../../utils/admin';
 
 export default defineEventHandler(async (event) => {
   await authenticateAdminRequest(event);
