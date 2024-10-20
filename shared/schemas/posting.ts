5da029aeba5229554f9c06e0aa7621d06a05528f
amandesai01 <amandesai01@gmail.com>
2024-10-20 21:37:37 +0530

    revert from update schema

--- shared/schemas/posting.ts
+++ shared/schemas/posting.ts
@@ -24,7 +24,6 @@ export const updateJobPostingSchema = z.object({
   contents: z.string().optional(),
   tagsCSV: z.string().optional(),
   isPublished: z.boolean(),
-  isExpired: z.boolean().optional(),
 });
 
 export const deleteJobPostingSchema = z.object({
