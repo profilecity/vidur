b8c698a7a79401a977cd4ecc27f31397aa1477f0
amandesai01 <amandesai01@gmail.com>
2024-10-23 10:54:43 +0530

    fix(posting): disable editing for expired postings

--- app/pages/admin/postings/edit.vue
+++ app/pages/admin/postings/edit.vue
@@ -28,6 +28,7 @@ const { handleSubmit, errors, defineField } = useForm({
 });
 
 const validTillCalendarDate = ref<CalendarDate>();
+const isExpired = ref<boolean>(false);
 
 const [id] = defineField('id');
 
@@ -50,6 +51,7 @@ validTill.value = (data.value.validTill as string | null) || undefined;
 if (validTill.value) {
   validTillCalendarDate.value = dateToCalendarDate(new Date(validTill.value));
 }
+isExpired.value = data.value.isExpired;
 
 const onSubmit = handleSubmit(async (values) => {
   await updateData(values);
@@ -67,12 +69,20 @@ watch(validTillCalendarDate, (calendarDate) => {
   if (!calendarDate) return;
   validTill.value = calendarDateToDate(calendarDate).toISOString();
 });
+
+const disableFields = computed(() => changing.value || isExpired.value);
 </script>
 
 <template>
   <div class="w-full max-w-9xl mx-auto">
+    <div
+      class="flex justify-center items-cente border-b border-zinc-200 bg-red-200 text-red-600 text-sm py-1"
+      v-if="isExpired"
+    >
+      Posting has expired. Editing is disabled.
+    </div>
     <!-- Page header -->
-    <div class="flex flex-col md:flex-row justify-between items-center mb-4 border-b border-zinc-200 p-4 bg-white">
+    <div class="flex justify-between items-center mb-4 border-b border-zinc-200 p-4 bg-white">
       <!-- Left: Title -->
       <div class="mb-4 sm:mb-0">
         <h2 class="text-md md:text-lg text-zinc-800 font-bold flex items-center">
@@ -82,26 +92,26 @@ watch(validTillCalendarDate, (calendarDate) => {
       </div>
       <!-- Right: Actions -->
       <div class="flex items-center space-x-3">
-        <InputDatePicker label="Expiry Date" v-model="validTillCalendarDate" />
+        <InputDatePicker label="Expiry Date" v-model="validTillCalendarDate" :disabled="disableFields" />
         <AbstractConfirmationBox
           title="Delete Posting?"
           content="You won't be able to undo this action. You will loose access to applicant list."
           @confirm="onDelete"
         >
           <template #input="{ open }">
-            <InputButton variant="destructive" size="icon" @click="open" :disabled="changing">
+            <InputButton variant="destructive" size="icon" @click="open" :disabled="disableFields">
               <Icon name="material-symbols:delete-outline" class="h-4 w-4" />
             </InputButton>
           </template>
         </AbstractConfirmationBox>
-        <InputSwitch label="Publish?" v-model="isPublished" />
+        <InputSwitch label="Publish?" v-model="isPublished" :disabled="disableFields" />
         <AbstractConfirmationBox
           title="Save Posting?"
           content="Are you sure you want to save the changes?"
           @confirm="onSubmit"
         >
           <template #input="{ open }">
-            <InputButton :disabled="changing" @click="open">
+            <InputButton :disabled="disableFields" @click="open">
               <div class="flex spece-x-2 items-center">
                 <Icon name="lets-icons:save" class="w-3 h-3 mr-1" />
                 <span>Save</span>
@@ -118,7 +128,7 @@ watch(validTillCalendarDate, (calendarDate) => {
           <InputText
             placeholder="Senior Software Engineer"
             label="Title"
-            :disabled="changing"
+            :disabled="disableFields"
             :error="errors.title"
             v-model="title"
           />
@@ -126,12 +136,12 @@ watch(validTillCalendarDate, (calendarDate) => {
             class="mt-4"
             placeholder="Remote, Full Time, San Fransisco"
             label="Tags (CSV)"
-            :disabled="changing"
+            :disabled="disableFields"
             v-model="tagsCSV"
           />
           <div class="mt-4">
             <label class="block text-sm font-medium mb-1" for="jobdescription">Job Description</label>
-            <Editor placeholder="We are looking for someone who can..." v-model="contents" />
+            <Editor placeholder="We are looking for someone who can..." v-model="contents" :read-only="disableFields" />
             <div class="text-xs mt-1 text-rose-500">{{ errors.contents }}</div>
           </div>
         </div>
