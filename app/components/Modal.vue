bb2bc7db148d1cc9e67e365cefcb40d0ac9e0e8e
amandesai01 <amandesai01@gmail.com>
2024-10-28 12:23:37 +0530

    chore(ui): allow class override in modal

--- app/components/Modal.vue
+++ app/components/Modal.vue
@@ -13,6 +13,7 @@ import {
 defineProps<{
   title?: string;
   description?: string;
+  classOverride?: string;
 }>();
 
 const isModalOpen = ref(false);
@@ -37,6 +38,7 @@ const close = () => {
       />
       <DialogContent
         class="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-[100]"
+        :class="classOverride"
       >
         <DialogTitle class="font-bold text-zinc-900 font-noto" v-if="title">{{ title }}</DialogTitle>
         <DialogDescription class="text-sm text-zinc-500" v-if="description">{{ description }}</DialogDescription>
