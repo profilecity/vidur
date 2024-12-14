d3ef5d7e5fe85cc46c5e4e66143b5e3476954c1f
rjsamra <rajkumarsamra@gmail.com>
2024-12-15 01:56:14 +0530

    chore(review-tags): display config

---
+++ app/utils/reviewtags-support.ts
@@ -0,0 +1,4 @@
+// pending, deferred, rejected, shortlisted, approved
+// Index represents parentId.
+export const reviewTagColorVsIndex = ['amber', 'fuchsia', 'rose', 'blue', 'green'];
+export const reviewTagGroupVsIndex = ['Pending', 'Deferred', 'Rejected', 'Shortlisted', 'Approved'];
