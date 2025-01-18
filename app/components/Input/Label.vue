5286789bb273cb4d30bdbafc0a025e94785f5105
amandesai01 <amandesai01@gmail.com>
2025-01-18 16:50:28 +0530

    chore(ui): label allow child

--- app/components/Input/Label.vue
+++ app/components/Input/Label.vue
@@ -11,10 +11,13 @@ defineProps<{
 </script>
 
 <template>
-  <Label class="block text-sm font-medium mb-1 text-zinc-900 font-noto" :class="labelClass" :for="id">
-    {{ label }}
-    <br v-if="sublabel" />
-    <span class="text-zinc-600 text-xs" v-if="sublabel">{{ sublabel }}</span>
-    <span class="text-xs ml-1 text-rose-500" v-if="error">{{ error }}</span>
-  </Label>
+  <div class="flex flex-col w-full">
+    <Label class="block text-sm font-medium mb-1 text-zinc-900 font-noto pl-0.5" :class="labelClass" :for="id">
+      {{ label }}
+      <br v-if="sublabel" />
+      <span class="text-zinc-600 text-xs" v-if="sublabel">{{ sublabel }}</span>
+      <span class="text-xs ml-1 text-rose-500" v-if="error">{{ error }}</span>
+    </Label>
+    <slot name="input" />
+  </div>
 </template>
