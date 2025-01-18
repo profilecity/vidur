81167ca72f25b1d0b4fbfb68d098222ed6397ce1
amandesai01 <amandesai01@gmail.com>
2025-01-18 16:50:01 +0530

    chore(ui): unstyled switch

--- app/components/Input/Switch.vue
+++ app/components/Input/Switch.vue
@@ -5,7 +5,6 @@ import { useVModel } from '@vueuse/core';
 const props = defineProps<{
   id?: string;
   modelValue?: boolean;
-  label?: string;
   disabled?: boolean;
 }>();
 
@@ -13,19 +12,14 @@ const switchState = useVModel(props, 'modelValue');
 </script>
 
 <template>
-  <div class="flex gap-2 items-center bg-zinc-100 p-1 rounded-md border border-zinc-200">
-    <label class="font-noto text-sm leading-none select-none" :for="id" v-if="label">
-      {{ label }}
-    </label>
-    <SwitchRoot
-      :id
-      v-model:checked="switchState"
-      class="w-[42px] h-[25px] focus-within:outline focus-within:outline-black flex bg-zinc-900/50 shadow-sm rounded-full relative data-[state=checked]:bg-zinc-900"
-      :disabled="disabled"
-    >
-      <SwitchThumb
-        class="block w-[21px] h-[21px] my-auto bg-white shadow-sm rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]"
-      />
-    </SwitchRoot>
-  </div>
+  <SwitchRoot
+    :id
+    v-model:checked="switchState"
+    class="w-10 h-[22px] focus-within:outline focus-within:outline-black flex bg-zinc-900/50 shadow-sm rounded-full relative data-[state=checked]:bg-zinc-700 focus:ring-0"
+    :disabled="disabled"
+  >
+    <SwitchThumb
+      class="block w-4 h-4 my-auto bg-white shadow-sm rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]"
+    />
+  </SwitchRoot>
 </template>
