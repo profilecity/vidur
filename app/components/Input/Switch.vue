c9a3577eab5ca9a2737019c2b5ca6ecafe2b5a32
amandesai01 <amandesai01@gmail.com>
2025-01-30 16:56:22 +0530

    chore(ui): switch

--- app/components/Input/Switch.vue
+++ app/components/Input/Switch.vue
@@ -15,11 +15,11 @@ const switchState = useVModel(props, 'modelValue');
   <SwitchRoot
     :id
     v-model:checked="switchState"
-    class="w-10 h-[22px] focus-within:outline focus-within:outline-black flex bg-zinc-900/50 shadow-sm rounded-full relative data-[state=checked]:bg-zinc-700 focus:ring-0"
+    class="w-10 h-[22px] flex bg-zinc-900/50 shadow-sm rounded-full relative data-[state=checked]:bg-zinc-700"
     :disabled="disabled"
   >
     <SwitchThumb
-      class="block w-4 h-4 my-auto bg-white shadow-sm rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]"
+      class="block w-4 h-4 my-auto bg-white shadow-sm rounded-full transition-transform duration-100 translate-x-1 will-change-transform data-[state=checked]:translate-x-[21px]"
     />
   </SwitchRoot>
 </template>
