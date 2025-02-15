93044006dbc150156c29b392eec1efa69aecb2a7
Shivam Sharma <meshivam81@gmail.com>
2025-02-15 12:17:50 +0530

    fix(db): proactively fail if error with db during startup (#233)

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -6,21 +6,27 @@ let client: pg.Client | null;
 let drizzleInstance: NodePgDatabase;
 
 export async function useDatabase() {
-  const config = useRuntimeConfig();
-  if (client && drizzleInstance) return drizzleInstance;
+  try {
+    const config = useRuntimeConfig();
+    if (client && drizzleInstance) return drizzleInstance;
 
-  if (!config.db.host) throw new Error('Missing db.host in runtime config');
+    if (!config.db.host) throw new Error('Missing db.host in runtime config');
 
-  client = new pg.Client({
-    ...config.db,
-    ssl: IS_DEV
-      ? false
-      : {
-          rejectUnauthorized: false, // TODO: fix this.
-        },
-  });
-  await client.connect();
+    client = new pg.Client({
+      ...config.db,
+      ssl: IS_DEV
+        ? false
+        : {
+            rejectUnauthorized: false, // TODO: fix this.
+          },
+    });
 
-  drizzleInstance = drizzle(client);
-  return drizzleInstance;
+    await client.connect();
+
+    drizzleInstance = drizzle(client);
+    return drizzleInstance;
+  } catch (error) {
+    console.error('Error setting up database', error);
+    process.exit(1);
+  }
 }
