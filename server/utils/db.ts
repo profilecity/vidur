984500d8152fdc64fee4df17d1afb6e86b131460
amandesai01 <amandesai01@gmail.com>
2025-02-15 06:47:01 +0000

    fix

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -6,25 +6,27 @@ let client: pg.Client | null;
 let drizzleInstance: NodePgDatabase;
 
 export async function useDatabase() {
-  const config = useRuntimeConfig();
-  if (client && drizzleInstance) return drizzleInstance;
-
-  if (!config.db.host) throw new Error('Missing db.host in runtime config');
-
-  client = new pg.Client({
-    ...config.db,
-    ssl: IS_DEV
-      ? false
-      : {
-          rejectUnauthorized: false, // TODO: fix this.
-        },
-  });
   try {
-    await client.connect();
-  } catch (error) {
-    throw new Error(`Failed to connect to the database: ${(error as Error).message}`);
-  }
+    const config = useRuntimeConfig();
+    if (client && drizzleInstance) return drizzleInstance;
 
-  drizzleInstance = drizzle(client);
-  return drizzleInstance;
+    if (!config.db.host) throw new Error('Missing db.host in runtime config');
+
+    client = new pg.Client({
+      ...config.db,
+      ssl: IS_DEV
+        ? false
+        : {
+            rejectUnauthorized: false, // TODO: fix this.
+          },
+    });
+
+    await client.connect();
+
+    drizzleInstance = drizzle(client);
+    return drizzleInstance;
+  } catch (error) {
+    console.error('Error setting up database', error);
+    process.exit(1);
+  }
 }
