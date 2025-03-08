d59eadd48cf87cfa31b05eda3063daab17c118bd
Aman Desai <39585600+amandesai01@users.noreply.github.com>
2025-03-08 12:49:02 +0530

    Update Form.vue

--- app/components/admin/Hooks/Form.vue
+++ app/components/admin/Hooks/Form.vue
@@ -67,7 +67,7 @@ defineExpose({
 </script>
 
 <template>
-  <form class="flex flex-col items-center space-y-1 mt-3 mb-1" @submit="onSubmit">
+  <form class="flex flex-col items-start space-y-1 p-2" @submit="onSubmit">
     <VInputText
       class="w-full"
       label="Title"
